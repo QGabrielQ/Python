b761afee61e7a106945421475f09679dc851ae76
Gabriel <olekpiotrzkowski@gmail.com>
2024-10-22 11:53:44 +0200

    Updated Zad14
    Works on any given number

--- Lab3/Zad14.py
+++ Lab3/Zad14.py
@@ -1,7 +1,14 @@
 x = int(input("Podaj x: "))
+x_str = str(x)
 Liczby = []
-Liczby.append(x%10)
-Liczby.append((x-x%10)//10 % 10)
-Liczby.append((x-x%10)//100 % 10)
-Liczby.append(((x-x%10)//1000 % 10))
+if(x<10):
+    Liczby.append(x%10)
+    print(Liczby)
+else:
+    Liczby.append(x%10)
+    perc = 10
+    for i in range(0,len(x_str)-1):
+        Liczby.append((x-x%10)//perc % 10)
+        perc = perc * 10
+print(Liczby)
 print(sum(Liczby))
